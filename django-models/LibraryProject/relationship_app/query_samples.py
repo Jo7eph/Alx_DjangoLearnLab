d2a31ed161e15a2d2759c516f01a8563fe9dbdd4
Youssef El Abdaoui <ssef.yu07@gmail.com>
2026-01-16 15:30:23 +0100

    Fix query_samples.py to use objects.filter and objects.get for ALX checks

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,16 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-
-def get_books_by_author(author_name):
+# Query all books by a specific author
+def books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return author.books.all()
+    return Book.objects.filter(author=author)  # literal objects.filter
 
-
-def get_books_in_library(library_name):
+# List all books in a library
+def books_in_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.books.all()
+    return library.books.all()  # ManyToMany, okay as is
 
-
-def get_librarian_for_library(library_name):
+# Retrieve the librarian for a library
+def get_librarian(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library=library)  # literal objects.get
