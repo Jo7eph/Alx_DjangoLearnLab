b88fb72a5bb7d43dc0e9c14f3b51e1e80b5b4c53
Youssef El Abdaoui <ssef.yu07@gmail.com>
2026-01-16 15:44:33 +0100

    Fix views and template paths for ALX checks

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Author, Book, Library
 
 # Function-based view: List all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    # Point to the correct template path
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view: Show library details with all books
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'  # Correct path
     context_object_name = 'library'
