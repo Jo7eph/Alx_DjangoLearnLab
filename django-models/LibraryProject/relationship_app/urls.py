bf59b35a73722e761246dbddb8b51055e0ad8484
Youssef El Abdaoui <ssef.yu07@gmail.com>
2026-01-16 15:54:54 +0100

    Add function and class-based views, URLs, and templates for relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,7 @@
-# LibraryProject/relationship_app/urls.py
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path("books/", views.list_books, name="list_books"),  # function-based view
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # class-based view
 ]
