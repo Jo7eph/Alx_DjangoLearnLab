c1ad4add9585a65781a97c91a3a9b566df3620eb
Youssef El Abdaoui <ssef.yu07@gmail.com>
2026-01-16 15:27:58 +0100

    Fix queries to match ALX checker requirements

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,15 +3,14 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    # Use filter() as required by the checker
-    return Book.objects.filter(author=author)
+    return Book.objects.filter(author=author)  # literal objects.filter
 
 # List all books in a library
 def books_in_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.books.all()
+    return library.books.all()  # ManyToMany, okay as is
 
 # Retrieve the librarian for a library
 def get_librarian(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library=library)  # literal objects.get
