ed32ab8d5e6d397ed0aa8fd2089445e5fb389274
Youssef El Abdaoui <ssef.yu07@gmail.com>
2026-01-19 11:13:45 +0100

    Implement authentication views, urls, and templates in django-models

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+# Mandatory imports for built-in auth views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path("books/", views.list_books, name="list_books"),  # function-based view
-    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # class-based view
+    # Built-in views with specified template names
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    # Custom registration view
+    path('register/', views.register, name='register'),
 ]
