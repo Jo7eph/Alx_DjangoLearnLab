b8f58f6f385c5928e6ed5c298944bcd224b9280c
Youssef El Abdaoui <ssef.yu07@gmail.com>
2026-01-30 16:40:04 +0100

    Fix: Included api.urls in project settings and mapped path in api/urls

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import BookList
 
 urlpatterns = [
+    # This maps the URL to the view using path()
     path('books/', BookList.as_view(), name='book-list'),
 ]
