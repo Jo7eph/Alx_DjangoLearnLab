376db25d0de3c642aed585702c7e6365943d9682
Youssef El Abdaoui <ssef.yu07@gmail.com>
2026-02-06 15:49:35 +0100

    Implement Author and Book models with nested serializers and validation

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Author model stores the name of book writers
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+# Book model stores title and year, linked to an Author
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    # Foreign Key establishes a one-to-many relationship: One author can have many books
+    author = models.ForeignKey(Author, related_name='books', on_year=models.CASCADE)
+
+    def __str__(self):
+        return self.title
