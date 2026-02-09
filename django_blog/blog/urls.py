deaacfd23fa62bde8101a4e4296ad5065cc9c633
Youssef El Abdaoui <ssef.yu07@gmail.com>
2026-02-09 20:16:00 +0100

    Add PostByTagListView and tag URL route

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,4 +6,5 @@ app_name = "blog"
 urlpatterns = [
     path("", views.home_view, name="home"),
     path("posts/", views.posts_view, name="posts"),
+    path("tags/<slug:tag_slug>/", views.PostByTagListView.as_view(), name="posts_by_tag"),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post
 from django.db.models import Q
+from django.views.generic import ListView
+from taggit.models import Tag
 
 def home_view(request):
     return render(request, "blog/home.html")
@@ -21,3 +23,17 @@ def posts_view(request):
         "posts": posts,
         "query": query
     })
+class PostByTagListView(ListView):
+    model = Post
+    template_name = "blog/posts_by_tag.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        tag_slug = self.kwargs.get("tag_slug")
+        tag = Tag.objects.get(slug=tag_slug)
+        return Post.objects.filter(tags__in=[tag]).distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tag"] = Tag.objects.get(slug=self.kwargs.get("tag_slug"))
+        return context
