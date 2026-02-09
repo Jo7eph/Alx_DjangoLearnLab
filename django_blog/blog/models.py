1f0ed5d96933f14e291603c419506f1156f9631f
Youssef El Abdaoui <ssef.yu07@gmail.com>
2026-02-09 20:14:04 +0100

    Add search functionality with title, content, and tag filtering

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    tags = TaggableManager()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from .models import Post
+from django.db.models import Q
 
 def home_view(request):
     return render(request, "blog/home.html")
 
 def posts_view(request):
-    posts = Post.objects.all().order_by("-id")
-    return render(request, "blog/posts.html", {"posts": posts})
+    query = request.GET.get("q")
+
+    if query:
+        posts = Post.objects.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(tags__name__icontains=query)
+        ).distinct()
+    else:
+        posts = Post.objects.all()
+
+    return render(request, "blog/posts.html", {
+        "posts": posts,
+        "query": query
+    })
