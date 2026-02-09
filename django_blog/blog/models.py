a0a3fa00811e85c93d70137ce5bbaf225f55230f
Youssef El Abdaoui <ssef.yu07@gmail.com>
2026-02-09 19:35:09 +0100

    Fix tagging system using django-taggit and reset broken migrations

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,25 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
-from django.utils.text import slugify
-
-class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=60, unique=True, blank=True)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            base = slugify(self.name)
-            slug = base
-            i = 1
-            while Tag.objects.filter(slug=slug).exists():
-                i += 1
-                slug = f"{base}-{i}"
-            self.slug = slug
-        return super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.name
+from taggit.managers import TaggableManager
 
 
 class Post(models.Model):
@@ -27,19 +8,19 @@ class Post(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
-    tags = models.ManyToManyField(Tag, blank=True, related_name="posts")
+    tags = TaggableManager(blank=True)
 
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        return reverse("post_detail", kwargs={"pk": self.pk})
-
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -31,14 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'blog',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    "blog",
+    "taggit",
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
