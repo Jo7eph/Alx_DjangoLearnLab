0a34d1e403f6426bc4886ca8f8f0719fcd2d3a26
Youssef El Abdaoui <ssef.yu07@gmail.com>
2026-02-09 15:52:25 +0100

    Add Post CRUD with CBVs and author-only edit/delete

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,22 @@
 from .views import register_view, profile_view, home_view
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from .views import (
+    register_view, profile_view, home_view,
+    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+)
 
 urlpatterns = [
     path("", home_view, name="home"),
+
+    # Posts CRUD
+    path("posts/", PostListView.as_view(), name="posts"),
+    path("posts/new/", PostCreateView.as_view(), name="post-create"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-edit"),
+    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+
+    # Auth
     path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("register/", register_view, name="register"),
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,10 +1,14 @@
 from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from .forms import RegisterForm, ProfileForm
+from .forms import RegisterForm, ProfileForm, PostForm
 from .models import Post
+
 
 def posts_view(request):
     posts = Post.objects.all().order_by("-published_date")
@@ -42,3 +46,48 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, "blog/home.html")
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    ordering = ["-published_date"]
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+    success_url = reverse_lazy("posts")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+    def get_success_url(self):
+        return reverse_lazy("post-detail", kwargs={"pk": self.object.pk})
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("posts")
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
