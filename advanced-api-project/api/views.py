538bf25560afe2e9fdad954291c53d2054772193
Youssef El Abdaoui <ssef.yu07@gmail.com>
2026-02-13 16:52:47 +0100

    Add filtering, ordering, and search for Book endpoints

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,5 @@
+from django_filters import rest_framework
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
