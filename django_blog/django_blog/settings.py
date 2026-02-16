1c6c65f2b22147d48496614097a9c6ed1277d875
Youssef El Abdaoui <ssef.yu07@gmail.com>
2026-02-16 11:43:01 +0000

    Update database configuration for environment variables

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -92,10 +92,15 @@ if os.environ.get("USE_SQLITE") == "1":
 else:
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.getenv("DB_NAME", BASE_DIR / "db.sqlite3"),
+        "USER": os.getenv("DB_USER", ""),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", ""),
+        "PORT": os.getenv("DB_PORT", ""),
     }
 }
+
 
 
 
