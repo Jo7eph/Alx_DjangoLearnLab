cb0d71710acf7c3be3276c03622ea58e83f0b5b4
Youssef El Abdaoui <ssef.yu07@gmail.com>
2026-02-16 13:12:52 +0000

    Add comment CRUD views, CommentForm, and required URLs

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -7,7 +7,9 @@ class CommentForm(forms.ModelForm):
         fields = ["content"]
 
     def clean_content(self):
-        content = self.cleaned_data.get("content", "").strip()
+        content = (self.cleaned_data.get("content") or "").strip()
         if not content:
             raise forms.ValidationError("Comment cannot be empty.")
+        if len(content) < 2:
+            raise forms.ValidationError("Comment is too short.")
         return content
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import get_object_or_404
-from django.urls import reverse
-from .models import Post, Comment
-from .forms import CommentForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import CreateView, UpdateView, DeleteView
+from django.shortcuts import get_object_or_404
+from django.urls import reverse, reverse_lazy
+
+from .models import Post, Comment
+from .forms import CommentForm
+
 
 class CommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
@@ -21,3 +23,28 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
 
     def get_success_url(self):
         return reverse("post-detail", kwargs={"pk": self.post_obj.pk})
+
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_form.html"
+
+    def test_func(self):
+        comment = self.get_object()
+        return comment.author == self.request.user
+
+    def get_success_url(self):
+        return reverse("post-detail", kwargs={"pk": self.get_object().post.pk})
+
+
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = "blog/comment_confirm_delete.html"
+
+    def test_func(self):
+        comment = self.get_object()
+        return comment.author == self.request.user
+
+    def get_success_url(self):
+        return reverse("post-detail", kwargs={"pk": self.get_object().post.pk})
